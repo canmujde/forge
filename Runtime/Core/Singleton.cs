84e486167deda316d85ecc8cf694bae274a4a234
can <43178831+canmujde@users.noreply.github.com>
2025-02-18 01:21:13 +0300

    Update Singleton.cs

--- Runtime/Core/Singleton.cs
+++ Runtime/Core/Singleton.cs
@@ -5,6 +5,6 @@ namespace Core
     public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
         private static T _instance;
-        public static T Instance => _instance ?? (_instance = FindObjectOfType<T>());
+        public static T Instance => _instance ??= FindObjectOfType<T>();
     }
 }
